cc1fd99d799bb5c0e61ee211feb5c97a3d4402fb
OtiAnn <anniebh30@gmail.com>
2014-12-07 18:18:49 +0800

    Fix links

--- config.rb
+++ config.rb
@@ -6,9 +6,9 @@
 
 activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
-  # blog.prefix = "blog"
+  blog.prefix = "blogtest"
 
-  blog.permalink = "/blogtest/{year}/{month}/{day}/{title}.html"
+  blog.permalink = "{year}/{month}/{day}/{title}.html"
   # Matcher for blog source files
   # blog.sources = "{year}-{month}-{day}-{title}.html"
   # blog.taglink = "tags/{tag}.html"
